b46c811176f3d5d39340d1f7064bebaf1b41b8d5
Jason Hargrove <rijaha@gmail.com>
2021-07-04 01:10:10 -0500

    Updated default layout.

--- views/layouts/Default.jsx
+++ views/layouts/Default.jsx
@@ -1,22 +1,38 @@
 const React = require('react');
 
 class DefaultLayout extends React.Component {
+
   render() {
+
     return (
+
       <html lang="en" dir="ltr">
+
           <head>
             <meta charSet="utf-8" />
-            {this.props.styles && this.props.styles.map((style) => {
-              return(
-                <link key={style.key} rel="stylesheet" href={style.href} />
-              )
-            })}
             <title>{this.props.title}</title>
+            <link rel="stylesheet" href="/css/app.css" />
           </head>
+
           <body>
+            <header>
+              <a href="/stickers">
+                <div className="logo">
+                  Radioactive Sticker Store
+                </div>
+              </a>
+
+                <div className="cart">
+                  Shopping Cart
+                </div>
+
+            </header>
+
             <h1>{this.props.title}</h1>
             {this.props.children}
+            
           </body>
+
       </html>
     )
   }
