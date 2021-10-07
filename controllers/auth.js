b86da593435dc71e15e8c305847074482cfcfb6f
Jason Hargrove <rijaha@gmail.com>
2021-10-06 21:54:21 -0500

    Added auth.js and users.js. Installed bcrypt and jsonwebtonen.

---
+++ controllers/auth.js
@@ -0,0 +1,13 @@
+// To create the methods to authenticate users.
+
+// Hashing function.
+
+// Verification.
+
+// Login Users.
+
+// Register Users.
+
+// Header authenticate.
+
+// JSON authenticate.
---
+++ controllers/users.js
@@ -0,0 +1,3 @@
+// I would make full crud here, but I'm lazy.
+// Crud without create.
+// Read, Update, Destroy.
