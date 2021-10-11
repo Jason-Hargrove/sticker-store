d056989d83f3f88f5569212d43d837db280ba6e6
Jason Hargrove <rijaha@gmail.com>
2021-10-11 14:13:05 -0500

    Cleaned up Defualt.jsx.

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,24 +1,18 @@
 const React = require('react');
 const DefaultLayout = require('./layouts/Default');
 
-
 class Index extends React.Component {
-  render() {
 
+  render() {
     const stickers = this.props.stickers;
 
     return (
       <DefaultLayout>
-
         <div className="products">
           {
             stickers.map((sticker, i) => {
               return (
-
                 <div className="products__box" key={sticker._id}>
-
-
-
                   <div className="imageBox">
 
                     <div className="imageInn">
@@ -26,17 +20,11 @@ class Index extends React.Component {
                     </div>
 
                     <div className="hoverImg">
-
                       <a href={`/stickers/${sticker._id}`}><img src="https://images.squarespace-cdn.com/content/v1/57902faa59cc68a958c59c03/1625664782050-AW77UNJMCXLORA4AYEYP/click-to-buy.png?format=2500w"/></a>
-                      {/* <a href={`/stickers/${sticker._id}`}></a> */}
                     </div>
-
                   </div>
 
-
-
                   <div className="product__info">
-
                       {/* Product Name */}
                       <div className="product__name">
                         {sticker.name}
@@ -54,12 +42,12 @@ class Index extends React.Component {
 
                       {/* Product Count In Stock */}
                       <div className="product__stockinstock">
-                      <div className="product__stock">
-                        <span className="product__count">{sticker.countInStock}</span>
-                      </div>
-                      <div className="product__instock">
-                        <label>In Stock</label>
-                      </div>
+                        <div className="product__stock">
+                          <span className="product__count">{sticker.countInStock}</span>
+                        </div>
+                        <div className="product__instock">
+                          <label>In Stock</label>
+                        </div>
                       </div>
 
                       {/* Product Protectiv Coating or Not */}
--- views/layouts/Default.jsx
+++ views/layouts/Default.jsx
@@ -1,13 +1,9 @@
 const React = require('react');
 
 class DefaultLayout extends React.Component {
-
   render() {
-
     return (
-
       <html lang="en" dir="ltr">
-
           <head>
             <meta charSet="utf-8" />
             <title>{this.props.title}</title>
@@ -16,53 +12,47 @@ class DefaultLayout extends React.Component {
 
           <body>
             <header>
+              <nav className="navbar">
+                {/* ===== Logo ===== */}
+                <div className="navbar_logo">
+                  <a href="/stickers">
+                  <h2>Radioactive Sticker Store</h2>
+                  </a>
+                </div>
 
+                {/* ===== Links ===== */}
+                <ul className="navbar__links">
+                  <li>
+                    <a href="">
+                      {/* Shopping Cart Icon. Font Awesome. */}
+                      <i className="fas fa-shopping-cart"></i>
+                      <span>
+                      Cart
+                      <span className="cartlogo__badge">0</span>
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="">
+                      {/* Back to the home page. */}
+                      Shop
+                    </a>
+                  </li>
+                </ul>
 
-
-
-
-            <nav className="navbar">
-    {/* ===== Logo ===== */}
-      <div className="navbar_logo">
-      <a href="/stickers">
-      <h2>Radioactive Sticker Store</h2>
-      </a>
-      </div>
-    {/* ===== Links ===== */}
-    <ul className="navbar__links">
-      <li>
-        <a href="">
-          {/* Shopping Cart Icon. Font Awesome. */}
-          <i className="fas fa-shopping-cart"></i>
-          <span>
-          Cart
-          <span className="cartlogo__badge">0</span>
-          </span>
-        </a>
-
-      </li>
-      <li>
-        <a href="">
-          {/* Back to the home page. */}
-          Shop
-        </a>
-      </li>
-    </ul>
-
-    <div className="hamburger__menu">
-    <div></div>
-    <div></div>
-    <div></div>
-
-    </div>
-  </nav>
+                {/* ===== Hamburger Menu ===== */}
+                <div className="hamburger__menu">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </nav>
             </header>
 
             <h1>{this.props.title}</h1>
             {this.props.children}
 
           </body>
-
       </html>
     )
   }
